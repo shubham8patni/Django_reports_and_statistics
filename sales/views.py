22a0923bdeead1311434a9618bb71d0e360bf9c3
shubham8patni <shubhampatni88@gmail.com>
2022-10-01 20:38:48 +0530

    add more dataframes by merging and grouping, start graphs in sales/utils.py tricky method to save graph as image in buffer and encode and  decode

--- sales/views.py
+++ sales/views.py
@@ -13,6 +13,7 @@ def home_view(request):
 
     if request.method == "POST":
         sale_df = None 
+        merged_df = None
         date_from = request.POST.get('date_from')
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
@@ -26,7 +27,7 @@ def home_view(request):
             sales_df['salesman_id'] = sales_df['salesman_id'].apply(get_salesman_from_id )
             sales_df['created'] = sales_df['created'].apply(lambda x: x.strftime('%Y-%m-%d'))
             sales_df['updated'] = sales_df['updated'].apply(lambda x: x.strftime('%Y-%m-%d'))
-            sales_df.rename({'customer_id' : 'customer', 'salesman_id' : 'salesman'}, axis=1, inplace=True)
+            sales_df.rename({'customer_id' : 'customer', 'salesman_id' : 'salesman', 'id': 'sales_id'}, axis=1, inplace=True)
             
             positions_data = []
             for sale in sale_qs:
@@ -40,10 +41,17 @@ def home_view(request):
                     }
                     positions_data.append(obj)
 
-            sales_df = sales_df.to_html
-
             positions_df = pd.DataFrame(positions_data)
+            merged_df = pd.merge(sales_df, positions_df, on = 'sales_id')
+            df = merged_df.groupby('transaction_id', as_index=False)['price'].agg('sum')
+
+
+            merged_df = merged_df.to_html
+
             positions_df = positions_df.to_html 
+            sales_df = sales_df.to_html
+            df = df.to_html
+            
             # print(positions_df)
             print("######################################")
         else:
@@ -54,6 +62,8 @@ def home_view(request):
         "form" : form,
         'sales_df' : sales_df,
         'positions_df' : positions_df,
+        'merged_df' : merged_df,
+        'df' : df
     }
     
     return render(request, 'sales/home.html', context)
