6c9a6ec02b78e0a824bdc0fa1ce5022eadd42f7f
shubham8patni <shubhampatni88@gmail.com>
2022-10-01 19:49:44 +0530

    used apply function to directly update data frame values using utils.py functions and format date-time

--- sales/views.py
+++ sales/views.py
@@ -1,8 +1,11 @@
+import imp
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
 import pandas as pd
+from .utils import get_customer_from_id, get_salesman_from_id
+from datetime import date, datetime
 # Create your views here.
 
 def home_view(request):
@@ -19,6 +22,12 @@ def home_view(request):
         if len(sale_qs) > 0:    
             print("######################################")
             sales_df = pd.DataFrame(sale_qs.values())
+            sales_df['customer_id'] = sales_df['customer_id'].apply(get_customer_from_id)
+            sales_df['salesman_id'] = sales_df['salesman_id'].apply(get_salesman_from_id )
+            sales_df['created'] = sales_df['created'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            sales_df['updated'] = sales_df['updated'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            sales_df.rename({'customer_id' : 'customer', 'salesman_id' : 'salesman'}, axis=1, inplace=True)
+            
             positions_data = []
             for sale in sale_qs:
                 for pos in sale.get_positions():
@@ -35,10 +44,10 @@ def home_view(request):
 
             positions_df = pd.DataFrame(positions_data)
             positions_df = positions_df.to_html 
-            print(positions_df)
+            # print(positions_df)
             print("######################################")
         else:
-            print('no data')
+            print('no data') 
 
 
     context = {
